d3fc556283593448b32905140009c6990052248e
Brandon Groves <brandon.groves@gmail.com>
2014-11-26 15:09:34 -0500

    changed the home page generation to get page by path since the title could be different than home

--- home.php
+++ home.php
@@ -1,14 +1,14 @@
 <?php get_header(); the_post();?>
 <?php
-$home = get_page_by_title('Home');
+$home = get_page_by_path('home');
 if (!$home) {
 	$home = $post; // get something...
 }
 ?>
 
 <main class="home">
-	<h1><?=get_bloginfo('name')?></h1>
-	<?=apply_filters('the_content', $home->post_content);?>
+	<h1><?php echo $home->post_title; ?></h1>
+	<?php echo apply_filters('the_content', $home->post_content); ?>
 </main>
 
 <?php get_footer(); ?>
