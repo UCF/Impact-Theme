ea5f1ccf9639acc5307d63b3a6b49e0400344a38
Brandon Groves <brandon.groves@gmail.com>
2014-12-05 16:20:38 -0500

    fixed update page title overlapping with header

--- single-update.php
+++ single-update.php
@@ -1,7 +1,7 @@
 <?php disallow_direct_load('single.php');?>
 <?php get_header(); the_post();?>
 
-<div class="container">
+<div class="container page-base">
 	<div class="row page-content" id="<?=$post->post_name?>">
 		<div class="span12">
 			<article>
