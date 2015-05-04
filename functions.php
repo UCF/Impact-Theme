7931de04dbae4b6baeefdea6209cdd4a247df08a
Jim Barnes <james.barnes@ucf.edu>
2015-05-04 10:26:21 -0400

    Styling changes.

--- functions.php
+++ functions.php
@@ -180,7 +180,7 @@ function display_social($url, $title) {
         	Share "<?php echo $title; ?>" on Linkedin
         </a>
         <a class="share-email" target="_blank" data-button-target="<?php echo $url; ?>" href="mailto:?subject=<?php echo $title; ?>&amp;body=Check out this story on www.ucf.edu.%0A%0A<?php echo $url; ?>" title="Share this story in an email">
-        	Sahre "<?php echo $title; ?>" in an email
+        	Share "<?php echo $title; ?>" in an email
         </a>
     </aside>
     <?php
