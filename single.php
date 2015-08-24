8a35ed603ab603f237e036b8b487e445d9030cae
Jo Dickson <jogreybill@gmail.com>
2015-08-24 15:39:13 -0500

    Removed references to non-existent below-the-fold

--- single.php
+++ single.php
@@ -1,6 +1,6 @@
 <?php disallow_direct_load('single.php');?>
 <?php get_header(); the_post();?>
-	
+
 	<div class="row page-content" id="<?=$post->post_name?>">
 		<div class="col-md-9">
 			<article>
@@ -10,12 +10,10 @@
 				<?php the_content();?>
 			</article>
 		</div>
-		
+
 		<div id="sidebar" class="col-md-3">
 			<?=get_sidebar();?>
 		</div>
 	</div>
-		
-	<?php get_template_part('includes/below-the-fold'); ?>
 
 <?php get_footer();?>
