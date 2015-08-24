7d53f759ad2f040811f4d4398096401e0420ba53
RJ Bruneel <rj.bruneel@ucf.edu>
2015-08-24 10:34:18 -0400

    Updated fonts styles on homepage

--- footer.php
+++ footer.php
@@ -1,30 +1,26 @@
-
-	<?php if ( !is_front_page() ): ?>
-		<footer>
-			<div class="container">
-				<div class="row">
-					<div class="col-md-12">
-						<nav>
-							<?=wp_nav_menu(array(
-								'theme_location' => 'nav-menu',
-								'container' => 'false',
-								'menu_class' => 'menu horizontal',
-								'menu_id' => 'footer-menu',
-								'fallback_cb' => false,
-								'depth' => 1
-								));
-							?>
-						</nav>
-						<p class="footer-cta"><?php echo get_cta_prefix(); ?> <?php echo get_cta_link(); ?></p>
-						<p class="footer-logo">
-							<a target="_blank" href="http://www.ucf.edu/">Go to ucf.edu</a>
-						</p>
-					</div>
+	<footer>
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12">
+					<nav>
+						<?=wp_nav_menu(array(
+							'theme_location' => 'nav-menu',
+							'container' => 'false',
+							'menu_class' => 'menu horizontal',
+							'menu_id' => 'footer-menu',
+							'fallback_cb' => false,
+							'depth' => 1
+							));
+						?>
+					</nav>
+					<p class="footer-cta"><?php echo get_cta_prefix(); ?> <?php echo get_cta_link(); ?></p>
+					<p class="footer-logo">
+						<a target="_blank" href="http://www.ucf.edu/">Go to ucf.edu</a>
+					</p>
 				</div>
 			</div>
-		</footer>
-	<?php endif; ?>
-
+		</div>
+	</footer>
 	</body>
 	<?="\n".footer_()."\n"?>
 </html>
