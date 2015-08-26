20cafc4347b170166bed9dbc337b0e5d4b417110
RJ Bruneel <rj.bruneel@ucf.edu>
2015-08-26 15:36:09 -0400

    Add link to scroll down

--- dev/home/home.js
+++ dev/home/home.js
@@ -2,7 +2,8 @@
 	var resizeTimer,
 		$lead = $('#lead'),
 		$video = $('#video'),
-		$featuredLink = $('.featured-link');
+		$featuredLink = $('.featured-link'),
+		documentWidth = $(window).width();
 
 	function resizeLead() {
 		clearTimeout(resizeTimer);
@@ -11,12 +12,21 @@
 		}, 250);
 	}
 
+	function onResize() {
+		// prevent resizing the lead on mobile horizontal scroll
+		// http://stackoverflow.com/questions/17328742/mobile-chrome-fires-resize-event-on-scroll
+		if (documentWidth !== $(window).width()) {
+			documentWidth = $(window).width();
+			resizeLead();
+		}
+	}
+
 	function toggleLinks() {
 		if (this.currentTime) {
 			$featuredLink.addClass('fade');
-			if (this.currentTime > 6) {
+			if (this.currentTime > 5) {
 				$featuredLink.eq(2).removeClass('fade');
-			} else if (this.currentTime > 3) {
+			} else if (this.currentTime > 2.75) {
 				$featuredLink.eq(1).removeClass('fade');
 			} else if (this.currentTime > 0) {
 				$featuredLink.eq(0).removeClass('fade');
@@ -35,11 +45,19 @@
 		$featuredLink.removeClass('fade');
 	}
 
+	function smoothScroll(e) {
+        e.preventDefault();
+		$('html, body').animate({
+			scrollTop: $($(this).attr('data-target')).offset().top
+		}, 500);
+	}
+
 	function setupEventHandlers() {
-		$(window).resize(resizeLead);
+		$(window).on('resize', onResize);
 		$video.on('timeupdate', toggleLinks);
 		$video.on('play', hideVideoStill);
 		$video.on('ended', showVideoStill);
+		$('.smooth-scroll').on('click', smoothScroll)
 	}
 
 	function init() {
