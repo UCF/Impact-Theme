03d08fc43fc83b261fa6416704d1f8c31c8b5816
Jo Dickson <jogreybill@gmail.com>
2015-08-26 08:42:24 -0500

    Removed the_content filter on uploaded html files and replaced with just do_shortcode and wptexturize (to avoid wpautop)

--- page.php
+++ page.php
@@ -3,7 +3,7 @@
 	the_post();
 	$html_id = get_post_meta( $post->ID, 'page_html', True );
 	$html_file = wp_get_attachment_url( $html_id );
-	$html = apply_filters('the_content', file_get_contents( $html_file ) );
+	$html = wptexturize( do_shortcode( file_get_contents( $html_file ) ) );
 ?>
 <main class="page page-base" id="<?php echo $post->post_name; ?>">
 	<?php echo $html; ?>
