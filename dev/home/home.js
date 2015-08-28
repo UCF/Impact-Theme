0ebe0b318dc4a892c018be405459d3038e897c98
RJ Bruneel <rj.bruneel@ucf.edu>
2015-08-28 16:49:26 -0400

    Fixed issue where image resizes on scroll with mobile devices.

--- dev/home/home.js
+++ dev/home/home.js
@@ -1,22 +1,35 @@
 (function () {
 	var resizeTimer,
-		$lead = $('#lead'),
-		$video = $('#video'),
-		$featuredLink = $('.featured-link'),
-		documentWidth = $(window).width();
+		$window ,
+		$lead,
+		$video,
+		$videoImage,
+		$featuredLink,
+		documentWidth;
+
+	function cacheObjects() {
+		$window = $(window);
+		$lead = $('#lead');
+		$video = $('#video');
+		$videoImage = $('#video-image');
+		$featuredLink = $('.featured-link');
+		documentWidth = $window.width();
+
+	}
 
 	function resizeLead() {
 		clearTimeout(resizeTimer);
 		resizeTimer = setTimeout(function() {
-			$lead.css("height", ($(window).height() - 50) + "px");
+			$lead.css("height", ($window.height() - 50) + "px");
 		}, 250);
 	}
 
 	function onResize() {
 		// prevent resizing the lead on mobile horizontal scroll
 		// http://stackoverflow.com/questions/17328742/mobile-chrome-fires-resize-event-on-scroll
-		if (documentWidth !== $(window).width()) {
-			documentWidth = $(window).width();
+		if (documentWidth !== $window.width()) {
+			setImageVideoSize();
+			documentWidth = $window.width();
 			resizeLead();
 		}
 	}
@@ -53,16 +66,44 @@
 	}
 
 	function setupEventHandlers() {
-		$(window).on('resize', onResize);
+		$window.on('resize', onResize);
 		$video.on('timeupdate', toggleLinks);
 		$video.on('play', hideVideoStill);
 		$video.on('ended', showVideoStill);
-		$('.smooth-scroll').on('click', smoothScroll)
+		$('.smooth-scroll').on('click', smoothScroll);
+	}
+
+	function setImageVideoSize() {
+		var windowWidth = $window.width(),
+			windowHeight = $window.height(),
+			aspectRatio = $videoImage.width() / $videoImage.height();
+
+		// Resize image/video
+		if ((windowWidth / windowHeight) < aspectRatio) {
+			$video.removeClass().css('height',windowHeight);
+			$videoImage.removeClass().css('height',windowHeight);
+		} else {
+		    $video.removeClass().css('width', windowWidth);
+			$videoImage.removeClass().css('width', windowWidth);
+		}
+
+		// Center image/video
+		if (windowWidth < $videoImage.width()) {
+			var leftOffset = -(($videoImage.width() - windowWidth) * .5);
+			$video.css('left', leftOffset);
+			$videoImage.css('left', leftOffset);
+		} else {
+			var topOffset = -(($videoImage.height() - windowHeight) * .5);
+			$video.css('top', topOffset);
+			$videoImage.css('top', topOffset);
+		}
 	}
 
 	function init() {
+		cacheObjects();
 		resizeLead();
 		setupEventHandlers();
+		setImageVideoSize();
 	}
 	$(init);
 } ());
