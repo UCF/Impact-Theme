097e393da9be9d0d8c06f4a8147c8130f86e2ae3
Jo Dickson <jogreybill@gmail.com>
2015-08-28 13:26:28 -0500

    Removed custom css/js enqueue (duplicate)

--- template-profile.php
+++ template-profile.php
@@ -2,15 +2,14 @@
 /**
  * Template Name: Profile
  */
-	get_header(); 
+	get_header();
 	the_post();
-	enqueue_custom_files();
 	$html_id = get_post_meta( $post->ID, 'page_html', True );
 	$html_file = wp_get_attachment_url( $html_id );
 	$html = wptexturize( do_shortcode( file_get_contents( $html_file ) ) );
 ?>
-<main class="page page-profile" id="<?echo $post->post_name?>">
+<main class="page page-profile" id="<?php echo $post->post_name; ?>">
 	<?php echo $html; ?>
 </main>
 
-<?php get_footer();?>
+<?php get_footer(); ?>
