aea2a561ecf260a5fec754f5def2d57b3e71d9fe
RJ Bruneel <rj.bruneel@ucf.edu>
2015-09-23 15:45:50 -0400

    Added timeout  for the file_get_contents to suppress timeout errors in the logs.

--- template-profile.php
+++ template-profile.php
@@ -6,7 +6,15 @@
 	the_post();
 	$html_id = get_post_meta( $post->ID, 'page_html', True );
 	$html_file = wp_get_attachment_url( $html_id );
-	$html = wptexturize( do_shortcode( file_get_contents( $html_file ) ) );
+
+	// Set a timeout
+	$opts = array('http' => array(
+						'method'  => 'GET',
+						'timeout' => FEED_FETCH_TIMEOUT
+	));
+	$context = stream_context_create($opts);
+
+	$html = wptexturize( do_shortcode( file_get_contents( $html_file, false, $context ) ) );
 ?>
 <main class="page page-profile" id="<?php echo $post->post_name; ?>">
 	<?php echo $html; ?>
